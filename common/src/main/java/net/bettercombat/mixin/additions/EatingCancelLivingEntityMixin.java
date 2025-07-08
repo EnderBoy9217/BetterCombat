aa063950267b067840d2f8ea8e6d2f2fe6160dfb
EnderBoy9217 <122132914+EnderBoy9217@users.noreply.github.com>
2025-07-07 23:37:30 -0400

    Eating is not cancelled by damage that bypasses armor

--- common/src/main/java/net/bettercombat/mixin/additions/EatingCancelLivingEntityMixin.java
+++ common/src/main/java/net/bettercombat/mixin/additions/EatingCancelLivingEntityMixin.java
@@ -3,7 +3,9 @@ package net.bettercombat.mixin.additions;
 import net.bettercombat.BetterCombat;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageType;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.tag.DamageTypeTags;
 import net.minecraft.util.UseAction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -19,7 +21,7 @@ public class EatingCancelLivingEntityMixin {
 
     @Inject(method = "damage" , at = @At("TAIL"), cancellable = true)
     public void cancelEating(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        if (BetterCombat.config.eatingInterruption ) {
+        if (BetterCombat.config.eatingInterruption && !source.isIn(DamageTypeTags.BYPASSES_ARMOR)) {
             LivingEntity self = (LivingEntity)(Object)this;
             this.activeItemStack.getItem();
             if (this.activeItemStack.getItem().getUseAction(this.activeItemStack) == UseAction.EAT || this.activeItemStack.getItem().getUseAction(this.activeItemStack) == UseAction.DRINK) {
